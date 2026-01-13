51fcd96b0dfd1cef6d7d666f7258f51678c62f98
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2026-01-12 18:24:56 -0800

    chore(DSAP.Game): use GCLatencyMode.SustainedLowLatency

--- source/DSAP.Game/Proxy.cs
+++ source/DSAP.Game/Proxy.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
 using System.Reflection;
+using System.Runtime;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Threading;
@@ -31,6 +32,8 @@ internal static class Proxy
 #pragma warning restore CA2255
     public static void Initialize()
     {
+        GCSettings.LatencyMode = GCLatencyMode.SustainedLowLatency;
+
         try
         {
             var systemPath = Environment.GetFolderPath(Environment.SpecialFolder.System);
